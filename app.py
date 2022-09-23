9956b572f2470b1236d4b8a6029046a71376dbd7
William Aguilar <wil-19-60@live.com>
2022-09-22 23:12:20 -0600

    Pushing app via HEROKU with github actions

--- app.py
+++ app.py
@@ -4,36 +4,110 @@ import pickle
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
+import joblib
+import requests
+
 
 app=Flask(__name__)
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class Logger(BaseEstimator, TransformerMixin):
+    def __init__(self, apply_log = True):
+        self.apply_log = apply_log
+        
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+        logX = X.copy()
+        
+        if self.apply_log:
+            logX = np.log1p(X)
+            return logX
+    
+        else: return X
+
 ## Load the model
-regmodel=pickle.load(open('regmodel.pkl','rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
+cancerDetection = joblib.load('cancerDetectionStakingModel_joblib')
+
+scalar= joblib.load('std_sca_joblib')
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    data=request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+@app.route('/benigno')
+def benigno():
+    return render_template('benigno.html')
+
+@app.route('/maligno')
+def maligno():
+    return render_template('maligno.html')
+
 
 @app.route('/predict',methods=['POST'])
 def predict():
+    print(request.form.values())
     data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
+
+    columns = ['radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean',
+       'smoothness_mean', 'compactness_mean', 'concavity_mean',
+       'concave points_mean', 'radius_se', 'texture_se', 'perimeter_se',
+       'area_se', 'smoothness_se', 'compactness_se', 'concavity_se',
+       'symmetry_se', 'fractal_dimension_se', 'radius_worst', 'texture_worst',
+       'perimeter_worst', 'area_worst', 'smoothness_worst', 'concavity_worst',
+       'concave points_worst', 'symmetry_worst']
+
+    tuned_names = ['Logistic Regression', 'SVC', 'GradientBoosting', 'RandomForest', 'KNNeighbors']
+
+    logreg_tuned = joblib.load('logreg_tuned_joblib')
+    std_sca = joblib.load('std_sca_joblib')
+    svc_tuned = joblib.load('svc_tuned_joblib')
+    gdb_tuned = joblib.load('gdb_tuned_joblib')
+    rf_tuned = joblib.load('rf_tuned_joblib')
+    knn_tuned = joblib.load('knn_tuned_joblib')
+
+    tuned_test_pred = np.zeros(shape=(1, 5)) # 5 models
+
+    test = pd.DataFrame(data=[data], columns=columns)
+
+    tuned_test_pred[:,0] = logreg_tuned.predict_proba(test)[:,1]
+    tuned_test_pred[:,1] = svc_tuned.predict_proba(test)[:,1]
+    tuned_test_pred[:,2] = gdb_tuned.predict_proba(test)[:,1]
+    tuned_test_pred[:,3] = rf_tuned.predict_proba(test)[:,1]
+    tuned_test_pred[:,4] = knn_tuned.predict_proba(test)[:,1]
+
+
+    tuned_test_pred = pd.DataFrame(data=tuned_test_pred, 
+                           columns=tuned_names)  
+
+    X_test_scaled = std_sca.transform(test)
+
+    X_test_final = np.concatenate([X_test_scaled, tuned_test_pred], axis=1)
+    
+    request_body = {
+    "Input": X_test_final.tolist()
+    }
+
+    print(request_body)
+    data = json.loads(json.dumps(request_body))
+    payload = json.dumps(data)
+
+    res = requests.post('https://6ua8sc2zr3.execute-api.us-east-1.amazonaws.com/PROD/api-ml-model', data = payload)
+
+    if(res.content == b'"B"'):
+        finalLabel = "TUMOR BENIGNO"
+    else:
+        finalLabel = "TUMOR MALIGNO"
+
+    return render_template("home.html",prediction_text="La prediccion final es: {}".format(finalLabel))
 
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=False)
    
      
